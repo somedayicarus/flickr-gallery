58b1b821da72e67a34ba4ce759e47ce4bffcaea7
Niki Noll <nicole.therese.ng@gmail.com>
2015-10-04 14:00:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
 	if (xhr.readyState === 4 && xhr.status === 200) {
         //parse api response
-		var parsed = JSON.parse(xhr.responseText);
-		var photoSet = parsed.photoset.photo;
-		var photoDiv = '';
+	var parsed = JSON.parse(xhr.responseText);
+	var photoSet = parsed.photoset.photo;
+	var photoDiv = '';
         var count = 0;
         //loop through json and add content to photoDiv
-		for (var i = 0; i < photoSet.length; i+=1) {
+	for (var i = 0; i < photoSet.length; i+=1) {
             var URL = photoSet[i].url_m;
             var title = photoSet[i].title;
             var alt = photoSet[i].description._content;
@@ -22,9 +22,9 @@ xhr.onreadystatechange = function () {
             photoDiv += '<div class="flex-item"><a href="#image-' + count + '"><img alt="' + alt + '"src="' + URL + '"></a><div class="overlay" id="image-' + count + '"><img alt="' + alt + '"src="' + URL + '"><div><h2>' + title + '</h2><a href="#image-' + (count - 1) + '"><i class="fa-3x fa fa-caret-left"></i></a><a href="#image-' + (count + 1) + '"><i class="fa-3x fa fa-caret-right"></i></a></div><a href="#" class="lb-close"><i class="fa fa-close fa-lg"></i></a></div></div>';
 		}
         //add photoDiv to page
-		document.getElementById('photos').innerHTML = photoDiv + '</div>';
+	document.getElementById('photos').innerHTML = photoDiv + '</div>';
         document.getElementById('header').innerHTML = parsed.photoset.title;
-		}
+	}
 };
 //open and send ajax request
 xhr.open('GET', requestURL, true);
