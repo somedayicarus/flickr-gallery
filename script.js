b89448123747b2a97e952e7d55d186edae247345
Niki Noll <nicole.therese.ng@gmail.com>
2015-10-04 14:12:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 //global variables
 var key = '7b2816bfbe36946e5509027ae6833eda';
 var userID = '136638860%40N02';
@@ -7,24 +8,24 @@ var requestURL = 'https://api.flickr.com/services/rest/?method=flickr.photosets.
 //ajax request to flickr api
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-	if (xhr.readyState === 4 && xhr.status === 200) {
-        //parse api response
-		var parsed = JSON.parse(xhr.responseText);
-		var photoSet = parsed.photoset.photo;
-		var photoDiv = '';
+    if (xhr.readyState === 4 && xhr.status === 200) {
+    //parse api response
+        var parsed = JSON.parse(xhr.responseText);
+        var photoSet = parsed.photoset.photo;
+        var photoDiv = '';
         var count = 0;
         //loop through json and add content to photoDiv
-		for (var i = 0; i < photoSet.length; i+=1) {
+        for (var i = 0; i < photoSet.length; i+=1) {
             var URL = photoSet[i].url_m;
             var title = photoSet[i].title;
             var alt = photoSet[i].description._content;
             count ++;
             photoDiv += '<div class="flex-item"><a href="#image-' + count + '"><img alt="' + alt + '"src="' + URL + '"></a><div class="overlay" id="image-' + count + '"><img alt="' + alt + '"src="' + URL + '"><div><h2>' + title + '</h2><a href="#image-' + (count - 1) + '"><i class="fa-3x fa fa-caret-left"></i></a><a href="#image-' + (count + 1) + '"><i class="fa-3x fa fa-caret-right"></i></a></div><a href="#" class="lb-close"><i class="fa fa-close fa-lg"></i></a></div></div>';
-		}
+        }
         //add photoDiv to page
-		document.getElementById('photos').innerHTML = photoDiv + '</div>';
+        document.getElementById('photos').innerHTML = photoDiv + '</div>';
         document.getElementById('header').innerHTML = parsed.photoset.title;
-		}
+    }
 };
 //open and send ajax request
 xhr.open('GET', requestURL, true);
